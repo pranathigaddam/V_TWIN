62cadb532c6aec085bdd80ff61358346e6dd3c4f
pranathigaddam <pranathireddy.gaddam26@gmail.com>
2020-12-05 22:25:37 +0530

    initial commit

---
+++ src/login/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/vela-blue/theme.css';
+import 'primereact/resources/primereact.css';
+import 'primeflex/primeflex.css';
+import { Card } from 'primereact/card';
+
+function Login() {
+    return(
+        <div>Login screeen</div>
+    )
+}
+
+export default Login;
